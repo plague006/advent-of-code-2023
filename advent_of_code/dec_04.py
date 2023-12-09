d7ec3b8c1fe4f04e1c06bb6fcbbdc1832465b4fc
mheguy <mheguy@stingray.com>
2023-12-08 23:51:18 -0500

    Add dec4

--- advent_of_code/dec_04.py
+++ advent_of_code/dec_04.py
@@ -1,8 +1,34 @@
+from collections import defaultdict
 from helper import get_input_file_lines
 
 
-def main(lines: list[str]):
-    ...
+def main(cards: list[str]) -> None:
+    card_count = defaultdict(lambda: 1)
+    p1_total = 0
+
+    for card_num, card in enumerate(cards, 1):
+        win_nums, our_nums = card.split(":")[1].split("|")
+
+        win_nums = [int(n) for n in win_nums.split(" ") if n != ""]
+        our_nums = [int(n) for n in our_nums.split(" ") if n != ""]
+
+        winning_matches = 0
+        for num in our_nums:
+            if num in win_nums:
+                winning_matches += 1
+
+        if winning_matches:
+            p1_total += 2 ** (winning_matches - 1)
+
+            for i in range(1, winning_matches + 1):
+                card_count[card_num + i] += card_count[card_num]
+
+    p2_total = 0
+    for i in range(1, len(cards) + 1):
+        p2_total += card_count[i]
+
+    print(p1_total)
+    print(p2_total)
 
 
 if __name__ == "__main__":
