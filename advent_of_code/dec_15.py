e11fc13978bbb1e07bb0bb98440a12bf8587efd4
mheguy <plague006@gmail.com>
2023-12-24 13:07:27 -0500

    Add dec15 p1

--- advent_of_code/dec_15.py
+++ advent_of_code/dec_15.py
@@ -3,7 +3,15 @@ from helper import get_input_file_lines, Timer
 
 @Timer()
 def main(lines: list[str]):
-    ...
+    total = 0
+    for chars in lines[0].split(","):
+        value = 0
+        for char in chars:
+            value = ((value + ord(char)) * 17) % 256
+
+        total += value
+
+    print(total)
 
 
 if __name__ == "__main__":
